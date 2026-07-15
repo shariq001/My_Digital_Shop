27f16ccf24320bf2020e2e09e4263f29e0d188c3
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2026-07-15 20:49:48 +0500

    Listing Update

--- src/config/categories.ts
+++ src/config/categories.ts
@@ -1,8 +1,11 @@
 export const CATEGORIES = [
   "Portfolio",
+  "Bundles",
   "SaaS Landing",
   "E-commerce",
-  "Admin Dashboard"
+  "Admin Dashboard",
+  "FAQ",
+  "404",
 ] as const;
 
 export type Category = typeof CATEGORIES[number];
